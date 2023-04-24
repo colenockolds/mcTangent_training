d7daa55bf136f0578cb31574080830d0ff802dd5
colenockolds <78436813+colenockolds@users.noreply.github.com>
2023-04-23 23:32:14 -0500

    Update load.py

--- load.py
+++ load.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def load_data():
     print('Loading Training Data...')
-    XY_mat = np.loadtxt('../src/training_data/coordinate.txt')
-    EToV = np.loadtxt('../src/training_data/EToV.txt')
-    data_test = np.loadtxt('../src/training_data/initial_cond_0.txt').T.reshape(1,401,1536)
+    XY_mat = np.loadtxt('training_data/coordinate.txt')
+    EToV = np.loadtxt('training_data/EToV.txt')
+    data_test = np.loadtxt('training_data/initial_cond_0.txt').T.reshape(1,401,1536)
     data_training = np.zeros((49,401,1536))
     for ic in range(1,50):
-        data_i = np.loadtxt('../src/training_data/initial_cond_' + str(ic) + '.txt').T
+        data_i = np.loadtxt('training_data/initial_cond_' + str(ic) + '.txt').T
         data_training[ic-1,:,:] = data_i
     print('Data Loaded!')
     print('Training Data Shape: ' + str(data_training.shape))
@@ -16,9 +16,9 @@ def load_data():
 
 def load_test():
     print('Loading Test Data...')
-    XY_mat = np.loadtxt('../src/training_data/coordinate.txt')
-    EToV = np.loadtxt('../src/training_data/EToV.txt')
-    data_test = np.loadtxt('../src/training_data/initial_cond_0.txt').T.reshape(1,401,1536)
+    XY_mat = np.loadtxt('training_data/coordinate.txt')
+    EToV = np.loadtxt('training_data/EToV.txt')
+    data_test = np.loadtxt('training_data/initial_cond_0.txt').T.reshape(1,401,1536)
     print('Data Loaded!')
     print('Test Data Shape: ' + str(data_test.shape))
     return XY_mat, EToV, data_test
